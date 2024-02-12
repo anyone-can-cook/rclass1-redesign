d4680d38d101301e7435987ebde960395388e119
Patricia Martin <pmar21995@gmail.com>
2024-02-12 11:31:18 -0800

    updates to autograder

--- problemsets/problemset1/assignment1_tests.r
+++ problemsets/problemset1/assignment1_tests.r
@@ -1,4 +1,5 @@
 library(testthat)
+library(stringr)
 
 # each call to test_that() produces one test
 # each test represents one point value
@@ -7,20 +8,42 @@ library(testthat)
 
 test_that("first (visible)", {
   
-  expect_equal( sum(myVector), 6) 
+  expect_true(str_detect(getwd(), "\\/problemset1")) 
+  expect_true(length(list.files()) > 5)
 
 })
 
 test_that("second (invisible)", {
 
-  expect_true(is.character(myString))
-  expect_true(nchar(myString) > 2)
+  expect_length(sum(str_detect(list.files(), "problemset1.pdf")), 1)
 
 })
 
 test_that("third", {
 
-  expect_equal(nrow(myDataFrame), 2)
-  expect_equal(myDataFrame[2,3], 700.01, tolerance=1e-3)
+  expect_equal(nrow(df_recruiting), 6)
+  expect_equal(typeof(df_recruiting), "list")
+  expect_equal(length(df_recruiting), 6)
+  expect_output(str(df_recruiting), "$ school_type", fixed = TRUE)
+  expect_output(names(df_recruiting), c("state_code","school_type","ncessch","name","address","city"))
+  
 
+})
+
+test_that("fourth", {
+  
+  expect_equal(sum(age), 93)
+  expect_equal(typeof(age), "double")
+  expect_equal(length(age), 4)
+  expect_output(str(age), "num [1:4]", fixed = TRUE)
+  expect_equal(sum(age), 93)
+  expect_output(sum(age2), NA)
+  expect_equal(sum(age2, na.rm = TRUE), 93)
+  expect_equal(typeof(tf), "logical")
+  expect_equal(length(tf), 6)
+  expect_output(str(tf), "logi [1:6]", fixed = TRUE)
+  expect_equal(sum(tf, na.rm = TRUE), 3)
+  expect_equal(mean(tf, trim = 0, na.rm = TRUE), 0.6)
+  expect_equal(mean(tf, 0, TRUE), 0.6)
+  
 })
